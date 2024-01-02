57386e5b213842ad6822e6d5c8225ef0549fe35a
TegarAN-0088 <tegaradimasnugroho@gmail.com>
2024-01-02 08:19:52 +0700

    Proyek Akhir Laravel

---
+++ resources/views/pelanggan/page/contact.blade.php
@@ -0,0 +1,69 @@
+@extends('pelanggan.layout.index')
+
+@section('content')
+    <div class="row mt-4 align-items-center">
+        <div class="col-md-6">
+            <div class="content-text">
+                PT TokoOnlineKita merupakan perusahaan teknologi Indonesia dengan misi pemerataan ekonomi secara digital di Indonesia. Visi
+                perusahaan adalah untuk menciptakan ekosistem dimana siapa pun bisa memulai dan menemukan apa pun. Hingga saat ini,
+                TokoOnlineKita termasuk marketplace yang paling banyak dikunjungi oleh masyarakat Indonesia.
+                
+                TokoOnlineKita turut mendukung para pelaku Usaha Mikro Kecil dan Menengah (UMKM) dan perorangan untuk mengembangkan usaha
+                mereka dengan memasarkan produk secara daring dengan Pemerintah dan pihak-pihak lainnya. Salah satu program kolaborasi
+                yang diinisasi oleh Tokopedia adalah acara tahunan MAKERFEST yang diadakan sejak bulan Maret 2018
+            </div>
+        </div>
+        <div class="col-md-6">
+            <img src="{{ asset('assets/images/office-2.png') }}" style="width:100%" alt="">
+        </div>
+    </div>
+
+    <div class="d-flex justify-content-lg-between mt-5">
+        <div class="d-flex align-items-center gap-4">
+            <i class="fa fa-users fa-2x"></i>
+            <p class="m-0 fs-5">+ 300 Pelanggan</p>
+        </div>
+        <div class="d-flex align-items-center gap-4">
+            <i class="fas fa-home fa-2x"> </i>
+            <p class="m-0 fs-5"> +500 Seller</p>
+        </div>
+        <div class="d-flex align-items-center gap-4">
+            <i class="fas fa-shirt fa-2x"></i>
+            <p class="m-0 fs-5">+ 300 Product</p>
+        </div>
+    </div>
+
+    <h4 class="text-center mt-md-5 mb-md-2">Contact Us</h4>
+    <hr class="mb-5">
+    <div class="row  mb-md-5">
+        <div class="col-md-5">
+            <div class="bg-secondary" style="width: 100%; height:50vh; border-radius:10px;"></div>
+        </div>
+        <div class="col-md-7">
+            <div class="card">
+                <div class="card-header text-center">
+                    <h4>Kritik dan saran</h4>
+                </div>
+                <div class="card-body">
+                    <p class="p-0 mb-5 text-lg-center">Masukan kritik dan saran anda kepada aplikasi kami ini agar kami dapat memberikan
+                        apa yang menjadi kebutuhan anda dan kami dapat berkembang lebih baik lagi.
+                    </p>
+                    <div class="mb-3 row">
+                        <label for="email" class="col-sm-2 col-form-label">Email</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="email" value=""
+                                placeholder="Masukan email Anda">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="pesan" class="col-sm-2 col-form-label">Pesan</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="pesan" placeholder="Masukan Pesan Anda">
+                        </div>
+                    </div>
+                    <button class="btn btn-primary mt-4 w-100"> Kirim pesan anda</button>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
